04a02892e81f0953bfd5d461fbb92d6c2ff73990
GoldenDude <edanazran@gmail.com>
2019-01-24 14:12:39 +0200

    removed not needed imports

--- CurrencyGUI.java
+++ CurrencyGUI.java
@@ -3,8 +3,6 @@ package com.azranozeri.finalproject;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.List;
